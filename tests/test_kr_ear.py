8d550585f9d2b32a25219d8dae87aa0177842a99
bhaskargautam <bhaskar.iiita@gmail.com>
2019-03-28 13:59:55 +0100

    Changed the Grid Search params for KR-EAR. still testing 32 cases.

--- tests/test_kr_ear.py
+++ tests/test_kr_ear.py
@@ -78,11 +78,11 @@ class Test_KR_EAR(unittest.TestCase):
         self._test_kr_ear(Census, self.get_default_params())
 
     def _test_grid_search(self, model):
-        dimension= [128]
-        batchSize= [1024]
-        learning_rate= [0.05, 0.1]
-        margin= [1, 10]
-        regularizer_scale = [0.05, 0.1]
+        dimension= [128, 256]
+        batchSize= [256, 512]
+        learning_rate= [0.1]
+        margin= [1, 0.1]
+        regularizer_scale = [0.1]
         epochs = [1000]
         neg_rel_rate = [1, 4]
         neg_rate = [1, 7]
