8d0b70c9184d6933a203133434d3e1a0ab97bbf0
bhaskargautam <bhaskar.iiita@gmail.com>
2019-04-01 12:12:36 +0200

    Modified params for Grid Search for SEEA. Restrcting to 32 tests.

--- tests/test_seea.py
+++ tests/test_seea.py
@@ -71,30 +71,31 @@ class Test_SEEA(unittest.TestCase):
     def test_seea_census(self):
         self._test_seea(Census, self.get_default_params())
 
-    def _test_grid_search(self, model, file_prefix):
-        beta = [6, 10]
-        dimension= [80, 120]
-        batchSize= [100]
-        learning_rate= [0.1, 0.2]
+    def _test_grid_search(self, dataset):
+        beta = [32]
+        dimension= [128, 1024]
+        batchSize= [32]
+        learning_rate= [0.1, 0.05]
         margin= [10, 1]
-        regularizer_scale = [0.1, 0.2]
-        epochs = [100, 500]
+        regularizer_scale = [0.1]
+        epochs = [500]
         neg_rel_rate = [1, 2]
         neg_rate = [1, 10]
-        max_iter = [10, 100]
+        max_iter = [10]
         count = 0
         max_fscore = 0
         max_prec_at_1 = 0
+        model = dataset()
 
-        logger = get_logger('RL.Test.GridSearch.SEEA.' + str(file_prefix))
+        logger = get_logger('RL.Test.GridSearch.SEEA.' + str(model))
 
         for b, d, bs, lr, m, reg, e, nr, nrr, mi in \
             itertools.product(beta, dimension, batchSize, learning_rate, margin, regularizer_scale, epochs, neg_rate, neg_rel_rate, max_iter):
-            params = {'beta': b, 'learning_rate': lr, 'margin': m, 'dimension': d, 'epochs': e, 'batchSize' : bs,
+            params = {'beta': b, 'learning_rate': lr, 'margin': m, 'dimension': d, 'max_epochs': e, 'batchSize' : bs,
                         'regularizer_scale' : reg, 'neg_rate' : nr, 'neg_rel_rate': nrr, 'max_iter' : mi}
-            logger.info("\nPARAMS: %s", str(params))
+            logger.info("\nTest:%d PARAMS: %s", count, str(params))
             count = count + 1
-            cur_fscore, cur_prec_at_1 = self._test_seea(model, file_prefix, params)
+            cur_fscore, cur_prec_at_1 = self._test_seea(dataset, params)
             if max_fscore <= cur_fscore:
                 max_fscore = cur_fscore
             if max_prec_at_1 <= cur_prec_at_1:
